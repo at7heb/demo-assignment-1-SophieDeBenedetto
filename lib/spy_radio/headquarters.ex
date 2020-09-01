0d5833cad5f4a490d8b64891870885cf8243ce6e
Howard Bussey <howard@tixlers.com>
2020-09-01 08:36:09 -0400

    step 2 completed: ATP

--- lib/spy_radio/headquarters.ex
+++ lib/spy_radio/headquarters.ex
@@ -1,2 +1,14 @@
 defmodule SpyRadio.Headquarters do
+  def wait_for_message(channel, queue, exchange) do
+    {:ok, _} = AMQP.Queue.declare(channel, queue, durable: true)
+    :ok = AMQP.Exchange.fanout(channel, exchange, durable: true)
+    :ok = AMQP.Queue.bind(channel, queue, exchange)
+
+    {:ok, _tag} =
+      AMQP.Queue.subscribe(channel, queue, fn payload, meta -> handle_message(payload, meta) end)
+  end
+
+  def handle_message(payload, _meta) do
+    IO.puts("Got super secret message: #{payload}")
+  end
 end
